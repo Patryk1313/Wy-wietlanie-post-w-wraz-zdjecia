0cc9a4f281a193f069ec359e638e5b169dbfed17
Patryk Kowalik <pacio.2002@go2.pl>
2022-03-23 20:34:10 +0100

    Add File

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Sora:wght@600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./style.css">
+    <title>Posty</title>
+</head>
+<body>
+
+<?php
+    include('connect.php');
+?>
+
+<?php
+    $showPosts = mysqli_query($conn, "SELECT title, text FROM posts;");
+    while($row = mysqli_fetch_array($showPosts)){
+        $title = $row['0'];
+        $text = $row['text'];
+    }
+?>
+
+<?php 
+    $showImg = mysqli_query($conn, "SELECT source FROM img WHERE ID = post.ID");
+    while($row = mysqli_fetch_array($showImg)){
+        $img = $row[0];
+    }
+?>
+
+    <section class='post-box'>
+        <h1><?php echo $title; ?></h1>
+        <p><?php echo $text; ?></p>
+        <img <?php echo "src= '$img'" ?> >
+    </section>
+
+</body>
+</html>
